0223a8b03b66f80eb3775ce097cb5b5b797ebfb3
meawong <megan.a.wong@gov.bc.ca>
2026-04-23 09:44:15 -0700

    31843 - Address PR comments

--- src/mixins/filing-template-mixin.ts
+++ src/mixins/filing-template-mixin.ts
@@ -141,12 +141,13 @@ export default class FilingTemplateMixin extends Mixins(AmalgamationMixin, DateM
     const filing: AmalgamationFilingIF = {
       header: {
         name: FilingTypes.AMALGAMATION_APPLICATION,
-        certifiedBy: this.getCertifiedBy,
         date: this.getCurrentDate,
         filingId: this.getFilingId,
         folioNumber: this.getFolioNumber || undefined,
         isFutureEffective: this.getEffectiveDateTime.isFutureEffective,
-        ...(this.isBaseCompany ? { authorizationReceived: this.getCertifyState.valid } : {})
+        ...(this.isBaseCompany
+          ? { authorizationReceived: this.getCertifyState.valid }
+          : { certifiedBy: this.getCertifyState.certifiedBy })
       },
       business: {
         legalType: this.getEntityType,
@@ -367,7 +368,6 @@ export default class FilingTemplateMixin extends Mixins(AmalgamationMixin, DateM
     const filing: ContinuationInFilingIF = {
       header: {
         name: FilingTypes.CONTINUATION_IN,
-        certifiedBy: this.getCertifiedBy,
         date: this.getCurrentDate,
         filingId: this.getFilingId,
         folioNumber: this.getFolioNumber || undefined,
@@ -907,7 +907,6 @@ export default class FilingTemplateMixin extends Mixins(AmalgamationMixin, DateM
     const filing: RestorationFilingIF = {
       header: {
         name: FilingTypes.RESTORATION,
-        certifiedBy: this.getCertifiedBy,
         date: this.getCurrentDate,
         filingId: this.getFilingId,
         folioNumber: this.getFolioNumber || undefined, // default FN; may be overwritten by staff BCOL FN
